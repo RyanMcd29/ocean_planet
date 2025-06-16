9305141134c794138b2f5c34d3d8975267ae9f2d
alexrjones123 <42883596-alexrjones123@users.noreply.replit.com>
2025-06-16 07:41:20 +0000

    Restored to '03b167ecc3fec8819b88153813589bdcdcb1acda'
    Replit-Restored-To: 03b167ecc3fec8819b88153813589bdcdcb1acda

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -60,12 +60,6 @@ const Header: React.FC = () => {
           )}>
             Learn
           </Link>
-          <Link href="/micro-lessons" className={cn(
-            "hover:text-[#05BFDB] transition duration-200",
-            isActive("/micro-lessons") && "text-[#05BFDB]"
-          )}>
-            Lessons
-          </Link>
 
         </nav>
         
